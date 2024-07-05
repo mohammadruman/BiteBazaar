51466205ab4c52a38101d0ec07c1ae19036d2e14
Mohammad Ruman <mohammaderuman@gmail.com>
2024-07-06 04:23:21 +0530

    Updated the code with Custom Hook for Fetching the Restaurant Menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,23 +1,12 @@
 import {useEffect,useState} from 'react'
 import Shimmer from './Shimmer';
 import {useParams} from 'react-router-dom'
-import { MENU_API_URL, MENU_API_REMAINING_URL } from '../../utils/constants';
+import useRestaurantMenu from '../../utils/useRestaurantMenu';
+
 const RestaurantMenu = () => {
     const {resId} = useParams();
-    console.log(resId)
-    const [resInfo,setresInfo] = useState(null);
-
-    useEffect(()=>{
-        fetchMenu();
-    },[])
-    const fetchMenu= async()=>{
-        const data = await fetch(MENU_API_URL+resId+MENU_API_REMAINING_URL);
-        const json = await data.json();
-       
-        setresInfo(json.data);
- console.log(json);
-       
-    };
+    const resInfo = useRestaurantMenu(resId); 
+   
    
  if(resInfo===null) return <Shimmer/>;
  const {itemCards} = resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[3].card.card;
---
+++ utils/useRestaurantMenu.js
@@ -0,0 +1,21 @@
+//in thi file we will fetch the data of the restaurant menu using a custom hook
+
+import { useEffect ,useState} from "react";
+import { MENU_API_URL, MENU_API_REMAINING_URL } from '../utils/constants';
+const useRestaurantMenu =(resId)=>{
+    //here we have created a local state to store the data of the restaurant menu   
+    const [resInfo,setresInfo] = useState(null);
+    //fetch data
+useEffect(()=>{
+fetchData();
+},[]);
+const fetchData= async()=>{
+    const data = await fetch(MENU_API_URL+resId+MENU_API_REMAINING_URL);
+    const json = await data.json();
+    setresInfo(json.data);
+};
+//return the data of the restaurant menu 
+    return resInfo;
+
+}
+export default useRestaurantMenu;
