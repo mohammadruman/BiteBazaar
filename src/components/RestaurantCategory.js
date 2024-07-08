1abbe1815bcdc8201d3f24b1039eea49b9a10bdc
Mohammad Ruman <mohammaderuman@gmail.com>
2024-07-08 16:25:36 +0530

    added a toggle Feature in restaurant menu for show items

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,17 +1,24 @@
 import ItemList from "./ItemList";
+import {useState} from 'react';
 
 const RestaurantCategory = ({data})=>{
+    const [showitems,setshowitems]=useState(false);
+    const handleclick=()=>{
+   setshowitems(!showitems);
+
+}
   
     return(
         <div>
              <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-        <div className="flex justify-between">
+        <div className="flex justify-between cursor-pointer" onClick={handleclick}>
+
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
           </span>
           <span>⬇️</span>
         </div>
-        <ItemList items={data.itemCards} />
+       {showitems &&  <ItemList items={data.itemCards} />}
       </div>
         </div>
        
